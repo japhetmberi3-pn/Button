7a3df2782d5f9940d55bcfe38f97f561e51a51ce
Speedy <michelmiyalou0@gmail.com>
2026-05-18 09:40:30 +0100

    Mise à jour des styles pour le thème sombre et ajustement des classes CSS des liens et boutons

--- app/Produit/page.tsx
+++ app/Produit/page.tsx
@@ -15,7 +15,7 @@ const z = useTransform(scrollY, [0,300], [0,150]); // mouvement de l'image
     return (
         <div>
             <div>
-                <div className="pt-5 bg-white h-30">
+                <div className="pt-5 bg-background h-30">
                     <nav className="flex items-center pl-19 w-full border-b border-gray-500 pb-2">
                         <motion.img drag
                         dragSnapToOrigin
@@ -33,10 +33,10 @@ const z = useTransform(scrollY, [0,300], [0,150]); // mouvement de l'image
                                     }
                                 }}
                             className="h-8 w-8" src="/images/Sans titre.png" alt="Menu burger" />
-                            <a href="Choisir spy" className="text-black pl-[37] pb-2">Choisir spy</a>
-                            <a href="Produits" className="text-black pl-[37] pb-2">Produits</a>
-                            <a href="Tarrifications" className="text-black pl-[37] pb-2">Tarrifications</a>
-                            <a href="Entreprise" className="text-black pl-[37] pb-2">Entreprise</a>
+                            <a href="Choisir spy" className="text-black dark:text-white pl-[37] pb-2">Choisir spy</a>
+                            <a href="Produits" className="text-black dark:text-white pl-[37] pb-2">Produits</a>
+                            <a href="Tarrifications" className="text-black dark:text-white pl-[37] pb-2">Tarrifications</a>
+                            <a href="Entreprise" className="text-black dark:text-white pl-[37] pb-2">Entreprise</a>
                         <motion.button drag
                         dragSnapToOrigin
                                whileHover={{scale: 1.3, backgroundColor:"#727272"}} 
@@ -44,7 +44,7 @@ const z = useTransform(scrollY, [0,300], [0,150]); // mouvement de l'image
                                 transition={{
                                     duration: 0.3
                                  }}
-                            className="text-black cursor pl-[50] ml-[250] rounded-full px-8 py-2 bg-black text-white">
+                            className="cursor pl-[50] ml-[250] rounded-full px-8 py-2 bg-black dark:bg-red-500 text-white">
                             Se connecter
                         </motion.button>
 
@@ -55,7 +55,7 @@ const z = useTransform(scrollY, [0,300], [0,150]); // mouvement de l'image
                                 transition={{
                                     duration: 0.3
                                 }}
-                            className="text-black cursor pl-[50] ml-3 rounded-full px-8 py-2 text-white bg-black">
+                            className="cursor pl-[50] ml-3 rounded-full px-8 py-2 text-white bg-black dark:bg-red-500">
                             Demarrer gratuitement
                         </motion.button>
 
